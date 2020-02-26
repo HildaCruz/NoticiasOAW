f980892d784e1f417c79c2d47296ff23c286c311
SamanthaCaamal <40219086+SamanthaCaamal@users.noreply.github.com>
2020-02-26 16:37:48 -0600

    Conexión a BD
    El archivo "INDEX" ya obtiene los datos de la BD.
    El archivo "SAVE" guarda la información de la página y los artículos.
    El archivo "noticias" es la BD.

--- index.php
+++ index.php
@@ -1,46 +1,54 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        require_once 'simplepie-1.5/autoloader.php';
-
-        $fp = fopen("archivo.txt", "r");
-        while (!feof($fp)) {
-            $linea = fgets($fp);
-        }
-        fclose($fp);
-
-        //$url = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
-        //http://feeds.weblogssl.com/xataka2.xml
-        $url = $linea;
-        $feed = new SimplePie();
-        $feed->set_feed_url($url);
-        $feed->init();
+        <title>RSS</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         
-        for ($i = 0; $i < $feed->get_items(); ++$i) {
-            $item = $feed->get_item($i);
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="peticion.js"></script>
+    </head>
+    <body style="margin: 60px;">
+        
+        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="busqueda" name="busqueda">Burcar</button> -->
 
-            $cadena = '<h1>' . $feed->get_title() . '</h1>';
-            $cadena .= '<p>' . $feed->get_description() . '</p>';
-            $cadena .= '<article>';
-            $cadena .= '<header>';
-            $cadena .= '<p>Title: <a href="' . $item->get_link() . '">' . $item->get_title() . '</a></p>';
-            $cadena .= '<p>Author: ' . $item->get_author()->get_name() . '</p>';
-            $cadena .= '<p>Date: ' . $item->get_date('Y-m-d H:i:s') . '</p>';
-            $cadena .= '<p>Description: ' . $item->get_description() . '</p>';
-            $cadena .= '</header>';
-            $cadena .= $item->get_content(true);
-            $cadena .= '</article><br><br><br>';
-            echo $cadena;
+        <nav class="navbar navbar-light bg-light" style="margin-bottom: 20px; margin-top: -50px;">
+            <a class="navbar-brand">Really Simple Syndication(RSS)</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+            </form>
+        </nav>
+
+        <?php
+        $mysqli = new mysqli('localhost', 'root', 'root', 'noticias');
+
+        if (!$mysqli) {
+            $info = "No se pudo realizar la conexión a la base de datos";
+        } else {
+            $query = "SELECT * FROM articulo";
+            $resultado = $mysqli->query($query);
+
+            if ($resultado->num_rows > 0) {
+                while ($aValues = $resultado->fetch_assoc()) {
+                    $link = $aValues['id'];
+                    $titulo = $aValues['titulo'];
+                    $autor = $aValues['autor'];
+                    $fecha = $aValues['fecha'];
+                    $descripcion = $aValues['descripcion'];
+
+                    $cadena = ' <article>';
+                    $cadena .= '<div class="card" style="margin: 20px;"> <h6 class="card-header"> <a href="' . $link . '">' . $titulo . '</a></h6>';
+                    $cadena .= '<div class="card-body"><p><em>Autor:</em> ' . $autor . '</p>';
+                    $cadena .= '<p class="card-text"><em>Fecha:</em> ' . $fecha . '</p>';
+                    $cadena .= '<p class="card-text"><em>Descripción:</em><br><div style="padding:15px; text-align: center;"> ' . $descripcion . '</div></p>';
+                    $cadena .= '</div></div>';
+                    $cadena .= '</article>';
+                    echo $cadena;
+                }
+            }
+            
+            $resultado->free();
+            $mysqli->close();
         }
         ?>
     </body>
