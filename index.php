cb19b2375443f37edf17034ac56d317258f7cb4a
Hilda Cruz <hcruz@walook.com.mx>
2020-02-26 17:46:53 -0600

    CSS general con filtro noticias

--- index.php
+++ index.php
@@ -18,22 +18,6 @@ require_once 'simplepie-1.5/autoloader.php';
                 var url = document.getElementById('_url').value;
                 alert(url);
             }
-            function toggleAC(){
-                var acc = document.getElementsByClassName("accordion");
-                var i;
-
-                for (i = 0; i < acc.length; i++) {
-                acc[i].addEventListener("click", function() {
-                    this.classList.toggle("active");
-                    var panel = this.nextElementSibling;
-                    if (panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                    } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                    } 
-                });
-                }
-            }
         </script>
     </head>
     <body>
