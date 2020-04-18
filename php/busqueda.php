1b5f39cfb34b7a868164214772fece61ee16fdc9
Luis Ojeda <luisma0330@hotmail.com>
2020-04-18 18:45:07 -0500

    Cambios de base y query de busqueda
    Cambios en la base de datos agregando un FULLTEXT en la base de datos y cambiando el query de la busqueda

--- php/busqueda.php
+++ php/busqueda.php
@@ -8,7 +8,7 @@
             if(!$mysqli){
                 $info = "No se pudo realizar la conexión a la base de datos";
             }else{
-                $query = "SELECT * FROM noticias WHERE MATCH (titulo, autor, descripcion) AGAINST ('$buscar' IN BOOLEAN MODE)";
+                $query = "SELECT * FROM articulos WHERE MATCH (titulo, autor, descripcion) AGAINST ('$buscar*' IN BOOLEAN MODE)";
                 $resultado = $mysqli->query($query);
                 if($resultado->num_rows > 0){
                     while ($aValues = $resultado->fetch_assoc()) {
@@ -17,14 +17,14 @@
                         $titulo = $aValues['titulo'];
                         $autor = $aValues['autor'];
                         $fecha = $aValues['fecha'];
-                        $hora = $aValues['hora'];
+                        //$hora = $aValues['hora'];
                         $descripcion = $aValues['descripcion'];
 
                         $cadena = ' <article>';
                         $cadena .= '<div class="card" style="margin: 20px;"> <h6 class="card-header"> <a href="' . $link . '">' . $titulo . '</a></h6>';
                         $cadena .= '<div class="card-body"><p><em>Autor:</em> ' . $autor . '</p>';
                         $cadena .= '<p class="card-text"><em>Fecha:</em> ' . $fecha . '</p>';
-                        $cadena .= '<p class="card-text"><em>Hora:</em> ' . $hora . '</p>';
+                        //$cadena .= '<p class="card-text"><em>Hora:</em> ' . $hora . '</p>';
                         $cadena .= '<p class="card-text"><em>Descripción:</em><br><div style="padding:15px; text-align: center;"> ' . $descripcion . '</div></p>';
                         $cadena .= '</div></div>';
                         $cadena .= '</article>';
